c0d42b231f29fe0a5a84b82a1184c7dfa47fb7fd
Alexander Rösler <7roesler@informatik.uni-hamburg.de>
2021-08-26 12:56:54 +0200

    update client for compiling

--- quoridor-client.rkt
+++ quoridor-client.rkt
@@ -200,27 +200,18 @@
     (lambda () (read-string 150)))))))
 
 ;ausgabe spielfeld im fenster
-(define (create-world worldname)
+(define (create-world)
   (big-bang new-game-2
     [to-draw render-state]
     [on-mouse mouse-action]
     [on-tick update-frame]
     [on-key key-press]
-    [register LOCALHOST] ;host-adress]
+    [register host-adress]
     [port custom-port]
     [state #f]
     [on-receive receive]
-    ;[name worldname]
+    [name "Quoridor"]
     )
   )
 
-; (main new-game-4)
-;;Macht zwei Welten auf
-
-
-(launch-many-worlds 
-  (create-world "Player 1")
-  (create-world "Player 2")
- ; (create-world "Player 3")
- ; (create-world "Player 4")
-  )
+(create-world)
